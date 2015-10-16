33834a683854b044a64183d169090f4107ca3e51
Anton Lebedevich <mabrek@gmail.com>
2015-10-17 00:27:05 +0300

    specify offset manually

--- src/main/scala/KafkaTimeseries.scala
+++ src/main/scala/KafkaTimeseries.scala
@@ -12,18 +12,11 @@ object KafkaTimeseries {
   def main(args: Array[String]) {
     val topic = args(0)
     val partition = Integer.parseInt(args(1))
+    val offset = java.lang.Long.parseLong(args(2))
     val topicAndPartition = new TopicAndPartition(topic, partition)
     val name = "client-" + topic + "-" + partition
     val consumer = new SimpleConsumer("localhost", 9092, 5000, BlockingChannel.UseDefaultBufferSize, name)
     try {
-      val offsetRequest = new OffsetRequest(Map(topicAndPartition -> 
-                                                new PartitionOffsetRequestInfo(OffsetRequest.EarliestTime, 1)))
-      val offsetResponse = consumer.getOffsetsBefore(offsetRequest)
-      if (offsetResponse.hasError) {
-        throw new Exception("offset request error code " + offsetResponse.partitionErrorAndOffsets(topicAndPartition).error)
-      }
-      val offset = offsetResponse.partitionErrorAndOffsets(topicAndPartition).offsets.head
-      System.out.println("start offset " + offset)
       val fetchRequest = new FetchRequestBuilder().clientId(name).addFetch(topic, partition, offset, 8096).build()
       val fetchResponse = consumer.fetch(fetchRequest)
       if (fetchResponse.hasError) {
