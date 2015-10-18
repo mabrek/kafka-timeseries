8818be3fd9cfe8f2e524996537bfe55a64625710
Anton Lebedevich <mabrek@gmail.com>
2015-10-18 16:42:59 +0300

    naive parquet writing, untested

--- build.sbt
+++ build.sbt
@@ -7,3 +7,7 @@ libraryDependencies += "org.apache.kafka" %% "kafka" % "0.8.2.2"
 libraryDependencies += "org.apache.parquet" % "parquet-column" % "1.7.0"
 
 libraryDependencies += "org.apache.parquet" % "parquet-hadoop" % "1.7.0"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.3.0"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.3.0"
--- src/main/scala/KafkaTimeseries.scala
+++ src/main/scala/KafkaTimeseries.scala
@@ -5,6 +5,18 @@ import kafka.common.TopicAndPartition
 import kafka.consumer.SimpleConsumer
 import kafka.message.MessageAndOffset
 import kafka.network.BlockingChannel
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.parquet.column.ParquetProperties.WriterVersion._
+import org.apache.parquet.example.data.Group
+import org.apache.parquet.example.data.simple.SimpleGroupFactory
+import org.apache.parquet.hadoop.ParquetWriter
+import org.apache.parquet.hadoop.ParquetWriter._
+import org.apache.parquet.hadoop.example.GroupWriteSupport
+import org.apache.parquet.schema.{Types, MessageType}
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName._
+import org.apache.parquet.schema.OriginalType._
+import org.apache.parquet.hadoop.metadata.CompressionCodecName._
 
 import scala.util.control.NonFatal
 
@@ -12,10 +24,21 @@ object KafkaTimeseries {
   def main(args: Array[String]) {
     val topic = args(0)
     val partition = Integer.parseInt(args(1))
-    val offset = java.lang.Long.parseLong(args(2))
+    val offset = args(2).toLong
     val topicAndPartition = new TopicAndPartition(topic, partition)
     val name = "client-" + topic + "-" + partition
     val consumer = new SimpleConsumer("localhost", 9092, 5000, BlockingChannel.UseDefaultBufferSize, name)
+    val configuration = new Configuration
+    val schema = Types.buildMessage()
+        .required(BINARY).as(UTF8).named("metric")
+        .required(DOUBLE).named("value")
+        .required(INT64).named("timestamp")
+      .named("GraphiteLine")
+    GroupWriteSupport.setSchema(schema, configuration)
+    val gf = new SimpleGroupFactory(schema)
+    val outFile = new Path("/tmp/graphite-parquet")
+    val writer = new ParquetWriter[Group](outFile, new GroupWriteSupport, UNCOMPRESSED, DEFAULT_BLOCK_SIZE, 
+      DEFAULT_PAGE_SIZE, 512, true, false, PARQUET_2_0, configuration)
     try {
       val fetchRequest = new FetchRequestBuilder().clientId(name).addFetch(topic, partition, offset, 8096).build()
       val fetchResponse = consumer.fetch(fetchRequest)
@@ -30,7 +53,15 @@ object KafkaTimeseries {
           val payloadBytes = new Array[Byte](mo.message.payloadSize)
           mo.message.payload.get(payloadBytes)
           val payload = new String(payloadBytes, StandardCharsets.US_ASCII)
-          System.out.println("key: " + key + " payload: " + payload)
+          try {
+            val split = payload.trim.split("\\s+")
+            val value = split(0).toDouble
+            val timestamp = split(1).toLong
+            writer.write(gf.newGroup().append("metric", key).append("value", value).append("timestamp", timestamp))
+          } catch {
+            case e:NumberFormatException =>
+              System.err.println(s"number format error $e in key: $key payload: $payload")
+          }
           Math.max(o, mo.nextOffset)
       }
       System.out.println("next offset " + nextOffset)
@@ -40,6 +71,7 @@ object KafkaTimeseries {
         e.printStackTrace()
     } finally {
       consumer.close()
+      writer.close()
     }
   }
 }
